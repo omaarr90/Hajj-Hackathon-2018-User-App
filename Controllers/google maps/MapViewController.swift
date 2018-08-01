ba5ed6ccac13225824e9d96bfae4377bd6e6060c
Abdulaziz Aloumi <eng.abdulazizalomi@gmail.com>
2018-08-01 14:10:41 +0300

    create maps controller

---
+++ Controllers/google maps/MapViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MapViewController.swift
+//  Hajj_Hackation_userApp
+//
+//  Created by Abdulaziz Aloumi on 01/08/2018.
+//  Copyright © 2018 X. All rights reserved.
+//
+
+import UIKit
+
+import GoogleMaps
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        // Do any additional setup after loading the view.
+    }
+
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Podfile
+++ Podfile
@@ -2,4 +2,6 @@ source 'https://github.com/CocoaPods/Specs.git'
 target 'Hajj_Hackation_userApp' do
 pod 'GoogleMaps'
 pod 'GooglePlaces'
+pod 'Alamofire', '~> 4.7'
+
 end
